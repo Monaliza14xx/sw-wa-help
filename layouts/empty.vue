f86c47dc22eadecb8e05c8e8528141269f5b7ac9
Monaliza14xx <mr.sounakhen@gmail.com>
2023-05-25 23:00:09 +0700

    first commit

---
+++ layouts/empty.vue
@@ -0,0 +1,75 @@
+<template>
+  <v-app dark>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+      <v-footer class="d-flex justify-center" :absolute="!fixed" app>
+        <center>
+          <strong
+            ><a id="text" href="https://www.facebook.com/ceitsoftware"
+              >CEIT Software</a
+            ></strong
+          >
+          <br />
+
+          <span> {{ auther }}</span>
+        </center>
+      </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {
+      auther: 'Develop by:Khonepaserth SOUNAKHEN'.toUpperCase(),
+
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
+<style>
+* {
+  font-family: 'boonhome';
+  color: #169fc6;
+}
+#btn {
+  font-family: 'boonhome';
+  color: #fff;
+}
+
+@font-face {
+  font-family: 'boonhome';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url('~assets/boonhome.otf') format('truetype');
+}
+html {
+  font-family: 'boonhome';
+}
+
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <center>
+    <img width="250px" src="@/assets/logo.jpeg" alt="" />
+    <h2 id="text">WhatsApp help</h2>
+    <p id="text">ພຽງແຕ່ທ່ານປ້ອນເບີໂທ ແລະ ຂໍ້ຄວາມ ກໍ່ສາມາດສົ່ງຂໍ້ຄວາມໄດ້</p>
+    <v-text-field
+      v-model="phoneNumber"
+      label="ເບີໂທລະສັບ"
+      placeholder="ກະລຸນາໃສ່ເບີໂທ ຕົວຢ່າງ: 020xxx ຫຼື 20xxx"
+      :rules="[{ required: true }]"
+      rounded
+      outlined
+      maxlength="11"
+    />
+    <v-text-field
+      v-model="msg"
+      label="ຂໍ້ຄວາມ"
+      placeholder="ກະລຸນາໃສ່ຂໍ້ຄວາມທີ່ຢາກສົ່ງ"
+      :rules="[{ required: true }]"
+      outlined
+      rounded
+    />
+    <v-btn
+      height="8vh"
+      width="100%"
+      depressed
+      rounded
+      @click="onSubmit"
+      color="#169fc6"
+      ><h3 class="white--text">ສົ່ງຂໍ້ຄວາມ</h3></v-btn
+    >
+  </center>
+</template>
+
+<script>
+export default {
+  layout: 'empty',
+  data() {
+    return {
+      code: '+856',
+      phoneNumber: null,
+      msg: null,
+      url: 'https://api.whatsapp.com/send',
+    }
+  },
+  methods: {
+    onSubmit() {
+      if (this.phoneNumber == null || this.msg == null) {
+        alert('ກະລຸນາປ້ອນເບີໂທ ແລະ ຂໍ້ຄວາມ')
+      } else {
+        window.location.href =
+          this.url +
+          '/?phone=' +
+          this.code +
+          this.phoneNumber +
+          '&text=' +
+          this.msg
+      }
+    },
+  },
+}
+</script>
+
+<style>
+</style>
