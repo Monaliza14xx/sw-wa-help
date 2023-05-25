6b262abd3712cef0b51a1de837466cadd16a8d71
Monaliza14xx <mr.sounakhen@gmail.com>
2023-05-25 23:10:16 +0700

    :fire:

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,8 +7,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - sw-whatsapp-helper',
-    title: 'sw-whatsapp-helper',
+    // titleTemplate: '%s - whatsapp-helper',
+    title: 'CEIT - Software_Whatsapp',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
